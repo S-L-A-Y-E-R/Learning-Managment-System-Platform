e36640b3804922ba593d0938af073e7cfe881098
ahmed <elshehry@gamil.com>
2023-09-20 13:12:28 +0300

    Added sidebar with no completed routes

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "./_components/side-bar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+                <Sidebar />
+            </div>
+            <main className="md:pl-56 h-full">
+                {children}
+            </main>
+        </div>
+    );
+}
+
+export default DashboardLayout;
